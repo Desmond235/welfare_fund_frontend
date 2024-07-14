1dc88c60e7f5b4fbf159256eeb894e0ce768c9a6
Desmond <adabedesmond@gmail.com>
2024-07-14 13:39:42 -0700

    added the option for user to select image

---
+++ lib/features/auth/widgets/signup/user_image_picker.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({super.key, required this.onPickedImage});
+  final void Function(File pickedImage) onPickedImage;
+
+  @override
+  State<UserImagePicker> createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File? _pickedImageFile;
+
+  void _pickedImage() async {
+    final pickedImage = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      maxWidth: 150,
+    );
+
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
+      _pickedImageFile = File(pickedImage.path);
+    });
+    widget.onPickedImage(_pickedImageFile!);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 40,
+            backgroundImage:
+                _pickedImageFile != null ? FileImage(_pickedImageFile!) : null,
+          ),
+          IconButton(
+            onPressed: _pickedImage,
+            icon: Icon(Icons.image),
+          )
+        ],
+      ),
+    );
+  }
+}
