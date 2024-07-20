7acf50bbccd75767089050418064f7489a4fc73d
Desmond <adabedesmond@gmail.com>
2024-07-20 08:19:34 -0700

    added bottom navigation bar control

---
+++ lib/core/components/bottom_nav_bar.dart
@@ -0,0 +1,38 @@
+import 'package:church_clique/core/base/main/main_page_provider.dart';
+import 'package:church_clique/core/components/nav_item.dart';
+import 'package:flutter/material.dart';
+import 'package:icons_flutter/icons_flutter.dart';
+import 'package:provider/provider.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int selectedIndex = 0;
+
+  @override
+  void initState() {
+    selectedIndex = 0;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      indicatorColor: Theme.of(context).colorScheme.primary,
+      selectedIndex: context.watch<MainPageProvider>().currentPage,
+      height: 60,
+      surfaceTintColor: Colors.white,
+      backgroundColor: Colors.white,
+      destinations:  [
+        NavItem(icon: MaterialCommunityIcons.home, index: 0),
+        NavItem(icon: Icons.payment, index: 1),
+        NavItem(icon: MaterialCommunityIcons.account, index: 2)
+      ],
+    );
+  }
+}
