d779ad560cdb68fa101f43cf7f61b2523a4897f4
Desmond <adabedesmond@gmail.com>
2024-07-21 13:34:04 -0700

    declared and initialize shared preferences

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -3,6 +3,7 @@ import 'package:church_clique/features/home/screens/home_screeb.dart';
 import 'package:church_clique/features/payment/views/payment_screen.dart';
 import 'package:church_clique/features/settings/views/settings_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final KMainPages = [
   HomeScreen(),
@@ -13,3 +14,5 @@ final KMainPages = [
 Color priCol(BuildContext context){
   return Theme.of(context).colorScheme.primary;
 }
+
+final Future<SharedPreferences> prefs =   SharedPreferences.getInstance();
