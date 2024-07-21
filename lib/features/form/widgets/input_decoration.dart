ee1c6ae88a8e7d7e202db680ed939372586b5e74
Desmond <adabedesmond@gmail.com>
2024-07-21 13:16:56 -0700

    added input control widget

---
+++ lib/features/form/widgets/input_decoration.dart
@@ -0,0 +1,20 @@
+import 'package:church_clique/core/constants/palette.dart';
+import 'package:flutter/material.dart';
+
+InputDecoration inputDecoration() {
+    return InputDecoration(
+      contentPadding: const EdgeInsets.all(10),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20),
+        borderSide: BorderSide(color: Palette.textColor1),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20),
+        borderSide: BorderSide(color: Palette.textColor1),
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20),
+        borderSide: BorderSide(color: Palette.textColor1),
+      ),
+    );
+  }
