7a40d2f915a14f252c95940046971d199ecb1a3a
Desmond <adabedesmond@gmail.com>
2024-07-25 14:48:22 -0700

    added send button component

---
+++ lib/core/components/send_button.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+SizedBox sendButton({
+  required BuildContext context,
+  required void Function() onTap,
+  required String text,
+  required IconData icon,
+  required MainAxisAlignment mainAxisAlignment
+}) {
+  return SizedBox(
+      width: double.infinity,
+      height: 55,
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primary.withOpacity(0.72),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Row(
+            mainAxisAlignment: mainAxisAlignment,
+            children: [
+              Text(
+                text,
+                style: TextStyle(color: Colors.white,fontSize: 15.2),
+              ),
+              Icon(icon, color: Colors.white.withOpacity(0.7))
+            ],
+          ),
+        ),
+      ));
+}
