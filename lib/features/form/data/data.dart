e7359cda27cf4366fac9e7597b7c455085c727ef
Desmond <adabedesmond@gmail.com>
2024-08-01 21:24:37 -0700

    added model to form

---
+++ lib/features/form/data/data.dart
@@ -0,0 +1,135 @@
+class GetData {
+  String fullName = '';
+  String dateOfBirth = '';
+  String dateOfRegistration = '';
+  String amountPaid = '';
+  String amountInWords = '';
+  String receiptNumber = '';
+  String contact = '';
+  String houseNo = '';
+  String placeOfAbode = '';
+  String landmark = '';
+  String homeTown = '';
+  String region = '';
+  String maritalStatus = '';
+  String others = '';
+  String nameOfSpouse ='';
+  String lifeStatus = '';
+  String numberOfChildren = '';
+  String namesOfChildren = '';
+  String occupation = '';
+  String fatherName = '';
+  String fLifeStatus = '';
+  String motherName = '';
+  String mLifeStatus =  '';
+  String nameOfNextKin = '';
+  String nextOfKinContact = '';
+  String classLeader = '';
+  String classLeaderContact = '';
+  String orgOfMember = '';
+  String orgLeaderContact = '';
+ 
+
+  void saveFullName(String? value){
+  fullName = value!;
+ }
+ void saveDateOfBirth(String? value){
+  dateOfBirth = value!;
+ }
+
+ void saveRegistrationDate(String? value){
+  dateOfRegistration = value!;
+ }
+
+ void saveAmountPaid(String? value){
+  amountPaid = value!;
+ }
+ void saveAmountPaidInWords(String? value){
+  amountInWords = value!;
+ }
+ void saveReceiptNumber (String? value){
+  receiptNumber = value!;
+ }
+ void saveContact(String? value){
+  contact = value!;
+ }
+ void saveHouseNumber(String? value){
+  houseNo = value!;
+ }
+ void savePlaceOfAbode (String? value){
+   placeOfAbode = value!;
+ }
+
+ void saveLandmark(String? value){
+  landmark = value!;
+ }
+ 
+ void saveHomeTown(String? value){
+  homeTown = value!;
+ }
+
+ void saveRegion(String? value){
+  region = value!;
+ }
+ void saveMaritalStatus(String? value){
+  maritalStatus = value!;
+ }
+ void saveOthers(String? value){
+  others = value!;
+ }
+ void saveNameOfSpouse(String? value){
+  nameOfSpouse = value!;
+ }
+ void saveLifeStatus(String? value){
+  lifeStatus = value!;
+ }
+ void saveNoChildren(String? value){
+  numberOfChildren = value!;
+ }
+
+void saveNameOfChildren(String? value){
+  namesOfChildren = value!;
+}
+ void saveOccupation(String? value){
+  occupation = value!;
+ }
+
+ void saveFatherName( String? value){
+  fatherName = value!;
+ }
+
+ void saveFLifeStatus(String? value){
+  fLifeStatus = value!;
+ }
+
+ void saveMotherName(String? value){
+  motherName = value!;
+ }
+
+ void saveMLifeStatus(String? value){
+   mLifeStatus = value!;
+ }
+
+ void saveNextOfKin(String? value){
+  nameOfNextKin =value!;
+ }
+ void saveNextOfKinContacts(String? value){
+  nextOfKinContact = value!;
+ }
+
+ void saveClassLeader(String? value){
+  classLeader = value!;
+ }
+
+ void saveClassLeaderContact(String? value){
+  classLeaderContact = value!;
+ }
+
+ void saveOrgOfMember(String? value){
+  orgOfMember = value!;
+ }
+
+ void saveOrgLeaderContact(String? value){
+  orgLeaderContact = value!;
+ }
+ }
