b09751a90b3088cd083811543f76d7b1038227e4
Desmond <adabedesmond@gmail.com>
2024-08-15 01:30:31 -0700

    added data row|

---
+++ lib/core/components/data_row.dart
@@ -0,0 +1,27 @@
+import 'package:church_clique/core/constants/constants.dart';
+import 'package:church_clique/features/form/models/membership_model.dart';
+import 'package:flutter/material.dart';
+
+List<DataCell> cells(MembershipModel memberData, bool editMode) {
+  return [
+    
+  ];
+}
+
+
+
+createTitleCell(String name, bool _isEditMode, {required void Function(String?) onSaved} ) {
+  return DataCell(
+    _isEditMode
+        ? TextFormField(
+            initialValue: name,
+            onSaved: onSaved ,
+            style: TextStyle(fontSize: 14),
+          )
+        : Text(name),
+  );
+}
+ 
+ 
+
+  
