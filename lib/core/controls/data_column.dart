b1de118c0baf8c09e0e490133ad4cb59a2a7fc3f
Desmond <adabedesmond@gmail.com>
2024-08-15 01:31:04 -0700

    added data column

---
+++ lib/core/controls/data_column.dart
@@ -0,0 +1,146 @@
+
+
+import 'package:flutter/material.dart';
+
+List<DataColumn> getColumns(){
+  return [
+     DataColumn(
+        label: Text(
+          'Date of Registration',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'contact',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'House Number',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Place of Abode',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Land Mark',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Home Town',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Region',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Marital Status',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Name Of Spouse',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Life Status',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      // DataColumn(
+      //   label: Text(
+      //     'contact',
+      //     style: TextStyle(color: Colors.white),
+      //   ),
+      // ),
+      // DataColumn(
+      //   label: Text(
+      //     'contact',
+      //     style: TextStyle(color: Colors.white),
+      //   ),
+      // ),
+      DataColumn(
+        label: Text(
+          'Occupation',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Father\'s Name',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Life Status',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Mother\'s Name',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Life Status',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Next of Kin',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Contact',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Class Leader',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Contact',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Work Place',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      DataColumn(
+        label: Text(
+          'Leader\'s Contact',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+  ];
+}
